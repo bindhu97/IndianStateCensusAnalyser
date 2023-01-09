7ce160dfca5ab98f3c95330290876f66db5d4d30
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-09 07:49:03 +0530

    [Bindhu]Add: Ability for the analyser to load the Indian States Census Information from a csv file

---
+++ IndianStateCensusAnalyser/CensusAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using IndianStateCensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA,
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+    }
+}
---
+++ IndianStateCensusAnalyserTest/CensusAnalyserTest.cs
@@ -0,0 +1,38 @@
+﻿using IndianStateCensusAnalyser;
+using IndianStateCensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IndianStateCensusAnalyser.CensusAnalyser;
+using static IndianStateCensusAnalyserTest.CensusAnalyserTest;
+
+namespace IndianStateCensusAnalyserTest
+{
+    public class CensusAnalyserTest
+    {
+        static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+        static string indianStateCensusFilePath = @"C:\Users\91973\source\repos\IndianStateCensusAnalyser\IndianStateCensusAnalyser\CSVFiles\StateCensusData.csv";
+
+        IndianStateCensusAnalyser.CensusAnalyser censusAnalyser;
+        Dictionary<string, CensusDTO> totalRecord;
+        Dictionary<string, CensusDTO> stateRecord;
+
+        [SetUp]
+        public void Setup()
+        {
+            censusAnalyser = new IndianStateCensusAnalyser.CensusAnalyser();
+            totalRecord = new Dictionary<string, CensusDTO>();
+            stateRecord = new Dictionary<string, CensusDTO>();
+        }
+
+        [Test]
+        public void GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount()
+        {
+            totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
+            Assert.AreEqual(29, totalRecord.Count);
+            Assert.AreEqual(37, stateRecord.Count);
+        }
+    }
+}
